1618507e411c2cbafbed356b660dd162632f2b52
kgor93 <mossme89@yahoo.com>
2013-04-27 15:10:07 -0300

    Update lab9.cpp
    compiles on my computer and works fine. Using Dev C++. Only thing is that the search doesn't work for 3 digit GPA's for some reason (i.e. 3.79). Otherwise, everything works.
    
    I was thinking about implementing a type checker in case someone inputs a string for an int (I've done a few times by accident), but that could be a whole number can of worms.

--- lab9.cpp
+++ lab9.cpp
@@ -1,4 +1,3 @@
-//test test
 #include <iostream>
 #include <iomanip>
 #include <string>
@@ -8,6 +7,7 @@
 
 using namespace std;
 
+void typeCheck(int);
 
 //student class
 class StudentTranscript{
@@ -95,6 +95,7 @@ char grade, inner, outer;
   {
            cout << "How many credits is the class?" << endl;
            cin >> credits;
+           typeCheck(credits);
            
            cout << "What is the student's grade for the class?" << endl;
            cout << "Enter A, B, C, D or E" << endl;
@@ -117,41 +118,60 @@ Student.push_back(temp);
    
    
 
-for(int i=0; i<Student.size(); i++)
-{
-cout<<setprecision(2)<<fixed<<showpoint<<"\nStudent: "<<Student[i].getName()<<" GPA: "<<Student[i].getGPA();
-}
+   for(int i=0; i<Student.size(); i++)
+   {
+   cout<<setprecision(2)<<fixed<<showpoint<<"\nStudent: "<<Student[i].getName()<<" GPA: "<<Student[i].getGPA() << endl;
+   }
    /////////////////Separating non-map part from map part
    
    //assignment Student elements to the map
-//   for (int i = 0; i < Student.size(); i++){
-//       Students.insert(pair<double, string>(Student[i].getGPA(), Student[i].getName()));        
-//   }
-//   
-//   
-//   cout << "Map size: " << Students.size() << endl << endl;
-//   
-//   cout << "What GPA do you want to search for? ";
-//   cin >> GPA;
-//   
-//   if (Students.count(GPA)==1){
-//       cout << Students.count(GPA) << " student has a GPA of " << GPA <<
-//       fixed << showpoint << setprecision(2) << endl << endl; //if statement for grammar
-//       
-//   }
-//   else{
-//       cout << Students.count(GPA) << " students have a GPA of " <<
-//       fixed << showpoint << setprecision(2) << GPA << endl << endl;
-//   }
-//   
-//   cout << "Here are GPA's of students in chronological order: " << endl << endl;
-//map<double, string>::iterator i;
-//   for(i=Students.begin(); i!=Students.end(); ++i)
-//   {
-//       cout << fixed << showpoint << setprecision(2) << (*i).second << " has a GPA of: " << (*i).first << endl;
-//   }
+   for (int i = 0; i < Student.size(); i++){
+       Students.insert(pair<double, string>(Student[i].getGPA(), Student[i].getName()));        
+   }
+   
+   
+   cout << "Map size: " << Students.size() << endl << endl;
+   
+   while (GPA != -1){
+   cout << "Enter a GPA to search for or -1 to quit: ";
+   cin >> GPA;
+   
+   if (GPA == -1){
+   break;
+           }
+   
+   if (Students.count(GPA)==1){
+       cout << Students.count(GPA) << " student has a GPA of " << GPA <<
+       fixed << showpoint << setprecision(2) << endl << endl; //if statement for grammar
+       
+   }
+   else{
+       cout << Students.count(GPA) << " students have a GPA of " <<
+       fixed << showpoint << setprecision(2) << GPA << endl << endl;
+   }
+}
+   
+   cout << "Here are GPA's of students in chronological order: " << endl << endl;
+   map<double, string>::iterator i;
+   for(i=Students.begin(); i!=Students.end(); ++i)
+   {
+       cout << fixed << showpoint << setprecision(2) << (*i).second << " has a GPA of: " << (*i).first << endl;
+   }
 
    
    system("pause");
    return 0;
 }
+
+void typeCheck(int num){
+     if (typeid(num) != typeid(int)){
+     cout << "Enter a GPA to search for or -1 to quit: ";
+     cin >> num;
+     }
+     else
+     if (!num){
+     cout << "Enter a GPA to search for or -1 to quit: ";
+     cin >> num;
+     }
+}
+
